af6237cf28ff7206896990e5e1fd4aee610f3d2a
Vann Heang <agnivann9@gmail.com>
2015-11-03 20:04:43 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = 'Thailand'
+  	@countries = ['Japan', 'France', 'Austrailia', 'New Zealand']
+  	@pics = ['france.jpg', 'japan1.jpg', 'new-zealand1.jpg', 'sydney1.jpg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
